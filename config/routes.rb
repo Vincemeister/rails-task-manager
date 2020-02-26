8b796be5f8f011b99e717146a63eb2f3d5d6dddd
Vincent Rehms <v.rehms@gmail.com>
2020-02-26 11:50:10 +0800

    partially completed rails-task-manager

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index', as: :all_tasks
+
+
+  get 'tasks/new', to: 'tasks#new'
+  get 'task/:id/edit', to: 'tasks#edit', as: :edit_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+
+  post 'tasks', to: 'tasks#create'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete "tasks/:id", to: "tasks#destroy"
+
+
+
+end
